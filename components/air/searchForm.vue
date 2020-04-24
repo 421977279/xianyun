8fcb495bed87da433b59caee875fd6dc5c0477fa
jackie <421977279@qq.com>
2020-04-24 20:13:41 +0800

    表单的验证

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -14,12 +14,14 @@
     </el-row>
 
     <!-- 机票搜索的表单 -->
-    <el-form class="search-form-content" ref="form" label-width="80px">
+    <el-form class="search-form-content" 
+		:rules="rules" 
+		ref="form" 
+		label-width="80px" 
+		:model="form">
       <!-- 出发城市的输入框 -->
-      <el-form-item label="出发城市">
-        <!-- fetch-suggestions：获取搜索建议，
-                它的功能就是根据输入的关键字，发起请求，
-        把请求的结果显示到下拉列表中-->
+      <el-form-item label="出发城市" prop="departCity">
+        <!-- fetch-suggestions：获取搜索建议，它的功能就是根据输入的关键字，发起请求，把请求的结果显示到下拉列表中-->
         <!-- select：选中下拉列表某一项时触发 -->
         <el-autocomplete
           :fetch-suggestions="queryDepartSearch"
@@ -32,26 +34,28 @@
       </el-form-item>
 
       <!-- 到达城市的输入框 -->
-      <el-form-item label="到达城市">
+      <el-form-item label="到达城市" prop="destCity">
         <el-autocomplete
           :fetch-suggestions="queryDestSearch"
           placeholder="请搜索到达城市"
           @select="handleDestSelect"
           class="el-autocomplete"
           v-model="form.destCity"
-					@blur="handleDestBlur"
+          @blur="handleDestBlur"
         ></el-autocomplete>
       </el-form-item>
-      <el-form-item label="出发时间">
+
+      <el-form-item label="出发时间" prop="departDate">
         <!-- change 用户确认选择日期时触发 -->
-        <el-date-picker 
-				type="date" 
-				placeholder="请选择日期" 
-				style="width: 100%;" 
-				@change="handleDate"
-				v-model="form.departDate"
-				value-format="yyyy-MM-dd"
-				:picker-options="pickerOptions"></el-date-picker>
+        <el-date-picker
+          type="date"
+          placeholder="请选择日期"
+          style="width: 100%;"
+          @change="handleDate"
+          v-model="form.departDate"
+          value-format="yyyy-MM-dd"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item label>
         <el-button style="width:100%;" type="primary" icon="el-icon-search" @click="handleSubmit">搜索</el-button>
@@ -76,7 +80,7 @@ export default {
 
       // 表单的数据
       form: {
-        depardtCity: "", //出发城市的输入框的值
+        departCity: "", //出发城市的输入框的值
         departCode: "", //出发城市的代码
         destCity: "", //到达城市的输入框的值
         destCode: "", //到达城市的代码
@@ -84,17 +88,31 @@ export default {
       },
 
       // 出发城市的下拉列表数据
-			departCities: [], //(空数组)
-			
-			// 到达城市的下拉列表数据
-			destCities: [],
+      departCities: [], //(空数组)
 
-			// 日期禁用选项
-			pickerOptions: {
-          disabledDate(time) {
-            return time.getTime() < Date.now() - 3600 * 1000 * 24;
-					}
-			}
+      // 到达城市的下拉列表数据
+      destCities: [],
+
+      // 日期禁用选项
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() < Date.now() - 3600 * 1000 * 24;
+        }
+      },
+
+      // 校验
+      // trigger默认是blur，但blur效果不佳，因此trigger的值可以随便填写
+      rules: {
+        departCity: [
+          { required: true, message: "请选择出发城市", trigger: "abc" }
+        ],
+        destCity: [
+          { required: true, message: "请选择到达城市", trigger: "abc" }
+        ],
+        departDate: [
+          { required: true, message: "请选择出发日期", trigger: "abc" }
+        ]
+      }
     };
   },
   methods: {
@@ -106,10 +124,15 @@ export default {
     // cb是回调函数(必须调用)，接收要展示的列表，接收的参数有个固定的格式
     // 参数必须是一个数组，并且数组是由对象组成的，并且对象必须又value属性
     queryDepartSearch(value, cb) {
+
       // 如果value的值为空，就不需要请求
       if (!value) {
         return;
-      }
+			}
+			
+			// 对部分表单字段进行校验的方法——validateField
+			// 监听输入框有值的时候重新验证表单，可以消除掉红的报错信息
+      this.$refs.form.validateField("departCity");
 
       // 请求和value相关的城市
       this.$axios({
@@ -146,10 +169,14 @@ export default {
     // 目标城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
     queryDestSearch(value, cb) {
+
       // 如果value的值为空，就不需要请求
       if (!value) {
         return;
-      }
+			}
+			
+			// 对部分表单字段进行校验的方法——validateField
+      this.$refs.form.validateField("destCity");
 
       // 请求和value相关的城市
       this.$axios({
@@ -165,9 +192,9 @@ export default {
         const newData = data.map(v => {
           v.value = v.name.replace("市", "");
           return v;
-				});
-				
-				// 将下拉列表数据保存到data空数组中，以便给blur事件失去焦点默认选中第一项时使用
+        });
+
+        // 将下拉列表数据保存到data空数组中，以便给blur事件失去焦点默认选中第一项时使用
         this.destCities = newData;
 
         //cb是要请求成功之后才调用，因为在这里才可以拿到数据
@@ -203,7 +230,11 @@ export default {
 
     // 提交表单是触发
     handleSubmit() {
-      console.log(this.form);
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          console.log(this.form);
+        }
+      });
     }
   },
   mounted() {}
