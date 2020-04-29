32ac27be23ee5298c40cccb85ef0e80132335b8a
jackie <421977279@qq.com>
2020-04-29 19:16:56 +0800

    添加分页的组件前端来分页

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -15,6 +15,21 @@
 					:key="index"
 					:data="item"/>
         </div>
+
+				<!-- size-change:切换单页条数 改变时会触发  -->
+				<!-- current-change:切换页面 改变时会触发  -->
+				<!-- current-page: 当前的页数 -->
+				<!-- page-sizes: 当前的条数 -->
+				<!-- total:总条数 -->
+				<el-pagination
+					@size-change="handleSizeChange"
+					@current-change="handleCurrentChange"
+					:current-page="pageIndex"
+					:page-sizes="[5, 10, 15, 20]"
+					:page-size="pageSize"
+					layout="total, sizes, prev, pager, next, jumper"
+					:total="total">
+				</el-pagination>
       </div>
 
       <!-- 侧边栏 -->
@@ -35,7 +50,13 @@ export default {
   data() {
     return {
 			// 总数据,里面包含了info,flights,total,options属性
-			flightsData:{}
+			flightsData:{},
+			// 当前的页数
+			pageIndex:1,
+			// 当前显示的条数
+			pageSize:5,
+			// 总页数
+			total:0
 		};
   },
   components: {
@@ -51,7 +72,17 @@ export default {
 			this.flightsData=res.data;
 			console.log(res.data);
     })
-  }
+	},
+	methods: {
+		// 切换单页条数时触发的事件
+		handleSizeChange(){
+
+		},
+		// 切换页数时触发的事件
+		handleCurrentChange(){
+
+		}
+	}
 };
 </script>
 
