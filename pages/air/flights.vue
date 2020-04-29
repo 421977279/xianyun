09f48e64b7ea394c4442a69f1cd421b6f2b06fbe
jackie <421977279@qq.com>
2020-04-29 17:42:20 +0800

    请求渲染航班列表

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,63 +1,72 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <FlightsListHead/>
-                
-                
-                <!-- 航班列表 -->
-                <div>
-                    <FlightsItem/>
-                </div>
-            </div>
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班列表 -->
+        <div>
+          <FlightsItem v-for="(item,index) in flightsData.flights"
+					:key="index"
+					:data="item"/>
+        </div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
 // 航班列表头部组件
-import FlightsListHead from "@/components/air/flightsListHead.vue"
+import FlightsListHead from "@/components/air/flightsListHead.vue";
 // 机票航班列表
-import FlightsItem from "@/components/air/flightsItem.vue"
+import FlightsItem from "@/components/air/flightsItem.vue";
 
 export default {
-    data(){
-        return {
-            
-        }
-    },
-    components: {
-        FlightsListHead,
-        FlightsItem
-    }
-}
+  data() {
+    return {
+			// 总数据,里面包含了info,flights,total,options属性
+			flightsData:{}
+		};
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+			// 总的数据,里面包含了info,flights,total,options属性
+			this.flightsData=res.data;
+			console.log(res.data);
+    })
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
